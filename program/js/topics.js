3a3cd8f288b85b7743b8ca2bb6d6f0475dacc0cb
Reuther <jere19ac@student.cbs.dk>
2019-11-08 12:51:12 +0100

    changed topics.js

--- program/js/topics.js
+++ program/js/topics.js
@@ -1,17 +1,21 @@
 class Topic{
-    constructor(topic, popCounter, id){
+    constructor(topic, popCounter, course, id){
         this.topic = topic;
         this.popCounter = popCounter;
+        this.course = course;
         this.id =id;
     }
 }
 // variable refering to the text-field
 var enteredTopic = document.getElementById("Topic");
+var courseField = document.getElementById('s1');
+// variable finding the selected course
+
 
 
 
 // Variable to get the id_generator function to work. It starts at 5, because we already hardcoded 5 topics
-var id_generator = 5;
+var id_generator = 10;
 // Function that creates a new id everytime it runs.
 function id_gen () {
     id_generator ++;
@@ -21,31 +25,67 @@ function id_gen () {
 var topic1 = new Topic(
     "Maksimeringsprincippet",
     0,
+    "Vos",
     1
 );
 var topic2 = new Topic(
     "Elasticitet",
     0,
-    2,
+    "Vos",
+    2
 );
 
 var topic3 = new Topic(
     "Isokvanter og MRTS",
     0,
+    "Vos",
     3,
 );
 var topic4 = new Topic(
     "MR=MC",
     0,
+    "Vos",
     4,
     );
 var topic5 = new Topic(
     "Fuldkommen konkurrence",
     0,
+        "Vos",
         5,
 );
+var topic6 = new Topic (
+    "Classes",
+    0,
+    "Pro",
+    6,
+);
+var topic7 = new Topic (
+    "Functions",
+    0,
+    "Pro",
+    7,
+);
+var topic8 = new Topic(
+    "Debugging",
+    0,
+    "Pro",
+    8,
+);
+var topic9 = new Topic (
+    "Scoping",
+    0,
+    "Pro",
+    9,
+);
+var topic10 = new Topic (
+    "Arrays",
+    0,
+    "Pro",
+    10,
+);
+
 //Puts the 5 preset topics into the hardcoded Topic.
-var hardTopics = [topic1,topic2,topic3,topic4,topic5];
+var hardTopics = [topic1,topic2,topic3,topic4,topic5, topic6, topic7, topic8, topic9, topic10];
 
 
 /*This function checks if allTopics is in local Storage.
@@ -63,6 +103,21 @@ checkLocalStorage();
 //allTopicsLS is here set to the topics in local storage. Here it also gathers the updated popCounter from localstorage.
 var allTopicsLS = JSON.parse(localStorage.getItem("allTopics"));
 
+var currentCourseTopics = [];
+
+function courseTopics() {
+    currentCourseTopics = [];
+    for (i= 0; i < allTopicsLS.length; i++){
+        var currentTopic = allTopicsLS[i];
+    if(courseField.options[courseField.selectedIndex].value == allTopicsLS[i].course){
+      currentCourseTopics.push(currentTopic);
+    }
+}}
+document.getElementById("s1").onchange = function(){
+    courseTopics();
+    displayValue();
+};
+
 /*DisplayValue is a function that sets the first 5 index in allTopicsLS to the 5 different buttons, and also makes sure that
 the topic in the index is written in the topic
  */
@@ -74,30 +129,30 @@ function displayValue() {
     var topic4Button = document.getElementById("topic4");
     var topic5Button = document.getElementById("topic5");
     //Changes the value of the buttons to the first 5 index's in the allTopicsLS array
-    topic1Button.value = allTopicsLS[0].topic;
-    topic2Button.value = allTopicsLS[1].topic;
-    topic3Button.value = allTopicsLS[2].topic;
-    topic4Button.value = allTopicsLS[3].topic;
-    topic5Button.value = allTopicsLS[4].topic;
+    topic1Button.value = currentCourseTopics[0].topic;
+    topic2Button.value = currentCourseTopics[1].topic;
+    topic3Button.value = currentCourseTopics[2].topic;
+    topic4Button.value = currentCourseTopics[3].topic;
+    topic5Button.value = currentCourseTopics[4].topic;
     //Onclick funtion that changes the text-field to the button
     topic1Button.onclick = function () {
-        enteredTopic.value = allTopicsLS[0].topic;
+        enteredTopic.value = currentCourseTopics[0].topic;
     };
     topic2Button.onclick = function () {
-        enteredTopic.value = allTopicsLS[1].topic;
+        enteredTopic.value = currentCourseTopics[1].topic;
     };
     topic3Button.onclick = function () {
-        enteredTopic.value = allTopicsLS[2].topic;
+        enteredTopic.value = currentCourseTopics[2].topic;
     };
     topic4Button.onclick = function () {
-        enteredTopic.value = allTopicsLS[3].topic;
+        enteredTopic.value = currentCourseTopics[3].topic;
     };
     topic5Button.onclick = function () {
-        enteredTopic.value = allTopicsLS[4].topic;
+        enteredTopic.value = currentCourseTopics[4].topic;
     };
 
 }
-displayValue();
+
 
 /*Function that checks if the topic already exists or not.
 the for statement makes sure that it runs through all the topics in the array.
@@ -105,7 +160,7 @@ And if the topic written in the text-field is equal to one of the topics in the 
  */
 function checkTopic () {
     var boolean = false;
-    for (i = 0; i < allTopicsLS.length; i++) {
+    for (i= 0; i < allTopicsLS.length; i++) {
         console.log("Antal gange loopet kører:" + i);
         if (enteredTopic.value == allTopicsLS[i].topic) {
             alert("Du har valgt en af de topics der eksisterer");
@@ -123,8 +178,9 @@ function checkTopic () {
             id_gen();
             var topicx = new Topic(
                 document.getElementById("Topic").value,
-                1);
-
+                1,
+                    );
+            topicx.course = courseField.options[courseField.selectedIndex].value;
             topicx.id = id_generator;
             console.log(topicx);
             allTopicsLS.push(topicx);
@@ -154,7 +210,6 @@ function executeTopics () {
        // localStorage.setItem("allTopics", JSON.stringify(allTopicsLS));
     //   console.log(JSON.parse(localStorage.getItem("allTopics")));
 localStorage.setItem("Topic", enteredTopic.value);
-
     };
 
 document.getElementById("submitBtn").addEventListener("click", executeTopics);
