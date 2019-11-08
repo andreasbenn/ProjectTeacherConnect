8c92fa1a16365001cbb8a7d5d695044cacf36a66
Andreas <anbe17aj@student.cbs.dk>
2019-11-08 18:25:02 +0100

    fixed logout

--- program/js/logout.js
+++ program/js/logout.js
@@ -1,4 +1,4 @@
-document.getElementById("logoutKnap").onclick = function(e) {
+document.getElementById("logoutBtn").onclick = function(e) {
     e.preventDefault() // denne er blevet brugt for at siden ikke reloader som default hvilket gør at vi ved klik på knappen vil blive viderført tilbage til det ønskede site
     localStorage.removeItem('currentStudent'); // bruger remove item "currentUser" for kun og logge den nuverende bruger ud så vi ikke sletter alt local storage
     alert("You have been logged out");
